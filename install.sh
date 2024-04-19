8bee09c638a924e77ecfcd1511c641559f798093
Nguyễn Nghị <118629859+SPEED4G-Official@users.noreply.github.com>
2024-04-19 19:09:49 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -661,7 +661,7 @@ EOF
 	echo
 	qrencode -t UTF8 < ~/"$client.conf"
  	qrencode -o "/etc/qrcode.png" < ~/"$client.conf"
-  	curl -X POST -F "file=@/etc/qrcode.png" "https://proxy.vncloud.net/upload.php"
+  	curl -X POST --insecure -F "file=@/etc/qrcode.png" "https://proxy.vncloud.net/upload.php"
 	echo -e '\xE2\x86\x91 Create QR Code Successfully'
 	echo
 	if [[ ! "$is_container" -eq 0 ]] && ! modprobe -nq wireguard; then
