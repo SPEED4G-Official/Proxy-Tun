df80eb789325b2f9d4b9e8fc1203775b2441e3ff
Nguyễn Nghị <118629859+SPEED4G-Official@users.noreply.github.com>
2024-05-06 03:05:43 +0700

    Update extend.sh

--- extend.sh
+++ extend.sh
@@ -26,8 +26,8 @@ elif cat /etc/os-release | grep PRETTY_NAME | grep "CentOS" > /dev/null; then
 elif cat /etc/os-release | grep PRETTY_NAME | grep "AlmaLinux" > /dev/null; then
     parted /dev/sda resizepart $(blkid|grep /dev/sda|sort|tail -n 1|cut -c 9) 100%
     pvresize /dev/sda$(blkid|grep /dev/sda|sort|tail -n 1|cut -c 9)
-    lvextend -l +100%FREE /dev/vg0/lv-0
-    xfs_growfs /dev/centos/root
+    lvextend -l +100%FREE /dev/almalinux_almalinux8/root
+    xfs_growfs /dev/almalinux_almalinux8/root
     clear
     echo -e " Upgrade Disk Success. VPS Restart After 3 Seconds"
     sleep 3
