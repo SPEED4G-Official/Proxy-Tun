4162a186ef85d2c14d644e8c316f2b9a035a85f6
Nguyễn Nghị <118629859+SPEED4G-Official@users.noreply.github.com>
2024-05-06 02:30:02 +0700

    Update extend.sh

--- extend.sh
+++ extend.sh
@@ -3,6 +3,9 @@ if cat /etc/os-release | grep PRETTY_NAME | grep "Debian" > /dev/null; then
     pvresize /dev/sda$(blkid|grep /dev/sda|sort|tail -n 1|cut -c 9)
     lvextend -l +100%FREE /dev/vg0/lv-0
     xfs_growfs /dev/vg0/lv-0
+    echo -e "Upgrade Disk Success. VPS Restart After 3 Seconds"
+    sleep 3
+    reboot
 elif cat /etc/os-release | grep PRETTY_NAME | grep "Ubuntu" > /dev/null; then
     parted /dev/sda resizepart $(blkid|grep /dev/sda|sort|tail -n 1|cut -c 9) 100%
     pvresize /dev/sda$(blkid|grep /dev/sda|sort|tail -n 1|cut -c 9)
