36380c8506879a4a9c3f7fce98db492eefed7fc0
Nguyễn Nghị <118629859+SPEED4G-Official@users.noreply.github.com>
2025-03-19 18:03:14 +0700

    Update add.sh

--- add.sh
+++ add.sh
@@ -3,3 +3,11 @@ sed -i s/^#.*baseurl=http/baseurl=http/g /etc/yum.repos.d/CentOS-*.repo
 sed -i s/^mirrorlist=http/#mirrorlist=http/g /etc/yum.repos.d/CentOS-*.repo
 yum clean all
 yum makecache
+yum update -y
+yum install nano -y
+echo "net.ipv6.conf.all.disable_ipv6 = 1" | sudo tee -a /etc/sysctl.conf
+echo "net.ipv6.conf.default.disable_ipv6 = 1" | sudo tee -a /etc/sysctl.conf
+echo "net.ipv6.conf.lo.disable_ipv6 = 1" | sudo tee -a /etc/sysctl.conf
+sudo sysctl -p
+cd /etc/sysconfig/network-scripts/
+# systemctl restart NetworkManager
